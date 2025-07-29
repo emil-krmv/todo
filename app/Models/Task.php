152c8fb53c0f905b859c8d53fac603426a76d5e3
emil-krmv <138843846+emil-krmv@users.noreply.github.com>
2025-07-30 00:49:49 +0300

    Add accessor/mutator for title field

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +18,18 @@ class Task extends Model
         'title', 'description', 'status',
         'priority', 'due_date'
     ];
+
+
+    /**
+     * Interact with the task's title.
+     */
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucfirst($value),
+            set: fn (string $value) => strtolower($value),
+        );
+    }
 
     /**
      * Scope a query to only include current user's tasks
